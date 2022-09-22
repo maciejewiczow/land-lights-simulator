731fbe8a5487406e2a1c5d940c6786c69a027ba5
Mafciejewicz <melonik0.9846@gmail.com>
2022-09-22 10:58:02 +0200

    Remove useLocation

--- src/views/LandingView/LandingView.tsx
+++ src/views/LandingView/LandingView.tsx
@@ -1,23 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router';
 import { AppState } from 'store';
 import { Lantern, SvgWrapper, Wrapper } from './parts';
 
 export const LandingView: React.FC = () => {
     const currentLight = useSelector((state: AppState) => state.currentLight);
-    const location = useLocation();
-
-    console.log(location);
 
     useEffect(() => {
-        const pathname = location.pathname.slice(0, -1);
+        const pathname = window.location.pathname.slice(0, -1);
         window.open(
             `${pathname}/controls`,
             'controls',
             'height=100,width=300,fullscreen=yes,toolbar=no,menubar=no,scrollbars=no,resizable=yes,location=no,directories=no,status=no',
         );
-    }, [location]);
+    }, []);
 
     return (
         <Wrapper>
